db30c513df0456d96e0ea6aeae0aebcfaf0a34a9
EmanuelW <emanuel.wahlqvist@proton.me>
2026-03-06 16:39:25 +0100

    Update build.sh
    Another attempt to fix cosmic install

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,7 +11,7 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 dnf5 install -y tmux
-dnf5 install -y cosmic-desktop
+dnf group install -y cosmic-desktop-environment
 
 # Use a COPR Example:
 #
