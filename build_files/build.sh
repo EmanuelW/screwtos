e5a1849b333a2251c87448e042fce861b045713b
Emanuel Wahlqvist <emanuel.wahlqvist@proton.me>
2026-06-06 16:52:43 +0200

    Naive attempt to install lemonade

--- build_files/build.sh
+++ build_files/build.sh
@@ -24,3 +24,6 @@ dnf5 install -y tmux nextcloud-client goose
 systemctl enable podman.socket
 
 ln -s /lib64/libbz2.so.1 /lib64/libbz2.so.1.0
+
+wget https://github.com/lemonade-sdk/lemonade/releases/download/v10.6.0/lemonade-server-10.6.0-fc43.x86_64.rpm
+dnf5 install ./lemonade-server-10.6.0-fc43.x86_64.rpm
